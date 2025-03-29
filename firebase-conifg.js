d7dfad6c65bda678de0e1e08eca2ac14345c9a8d
rohitmichael-alt <rohitmichael13@gmail.com>
2025-03-29 20:30:11 +0530

    Your commit message here

---
+++ firebase-conifg.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyA4iSeQgqKj-KT1QUzyhIbBSL5-qI_HOGA",
+    authDomain: "college-818ff.firebaseapp.com",
+    projectId: "college-818ff",
+    storageBucket: "college-818ff.firebasestorage.app",
+    messagingSenderId: "597321515803",
+    appId: "1:597321515803:web:f920582b9de978110f49be",
+    measurementId: "G-JTMQ5BWYR2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
